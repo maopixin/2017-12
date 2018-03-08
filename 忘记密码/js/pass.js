57308a5c27b2f2c6864f8a7141eb3034a0044133
dlzx-1 <dlzx-1@dlzx-1deMacBook-Air.local>
2018-03-08 18:08:33 +0800

    0

--- 忘记密码/js/pass.js
+++ 忘记密码/js/pass.js
@@ -1,45 +1,81 @@
-var arr = ['信息提交成功，请至您的邮箱点击激活码确认。','输入不能为空。','请输入正确的邮箱地址。',"新密码格式设置错误。","两次输入的密码不相同，请检查。","请输入新密码，再进行提交"]
+var arr = ['信息提交成功，请至您的邮箱点击激活码确认。','输入不能为空。','请输入正确的邮箱地址。',"新密码格式设置错误。","两次输入的密码不相同，请检查。","请输入新密码，再进行提交","提交过程中出现了一些问题，请您稍后重试。"]
 
+// 验证邮箱按钮提交时
 $('#updata').click(function(){
-    check();
-})
-
-// 倒计时然后跳转
-function outUrlStart(){
-    var timer = 0;
-    timer = setInterval(function(){
-        console.log($('#time').html())
-        $('#time').html(Number($('#time').html())-1);
-        if(Number($('#time').html())===0){
-            // 这里是要跳转的页面
-            window.location.href='http://www.dljy.com';
-        }
-    },1000);
-};
-
-// 验证邮箱
-function check(){
     var reg = new RegExp("^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$"); //正则表达式
     var obj = document.getElementById("use-name"); //要验证的对象
     if(obj.value === ""){
         //输入不能为空
-        showTips(1)
+        showTips(1);
         return false;
     }else if(!reg.test(obj.value)){
         //验证不通过
-        showTips(2)
+        showTips(2);
         return false;
     }else{
         //通过
-        showTips(3)
-        return true;
+        $.ajax({
+            url:"http://account.dljy.com/user/login/email_send",
+            data:{
+                email:$('#use-name').val().trim()
+            },
+            type:"POST",
+            success(data){
+                if(data.status.code===0){
+                    // 提交成功
+                    showTips(3);
+                }else{
+                    showTips(6);
+                }  
+            },
+            error(){
+                showTips(6);
+            }
+        });
     };
-};
+})
+
+// 修改密码 按钮提交时
+$('#resetpassword').click(function(){
+    if(testPassword("password")){
+        // 合法
+        if($('#password').val()===$('#repassword').val()){
+            // 审核相同，可以提交
+            // 拿到隐藏得邮箱地址
+            var email_url = $('#eamil').html();
+            $.ajax({
+                url:"",
+                data:{
+
+                },
+                success(data){
+                    if(data.status.code===0){//success  登录 然后跳转 第三步  ， 第三部自动跳转
+                        
+                    }else{
+                        showTips(6);
+                    }
+                },
+                error(){
+                    showTips(6);
+                }
+            })
+        }else{
+            // 审核失败，密码不同
+            showTips(4,2);
+            return false;
+        }
+    }else{
+        // 不合法
+        showTips(3,1);
+        return false;
+    };
+});
 
 // 弹窗
 
 function showTips(num,type){
     type=type?type:0;
+    console.log(type)
 	var div = document.createElement('div');
 	div.id = "jump-box";
 	div.style = "display:flex;position:fixed;z-index:99999;top:0;left:0;right:0;bottom:0;background-color:rgba(0, 0, 0, 0.5)";
@@ -53,40 +89,24 @@ function showTips(num,type){
 			            </div>
 			        </div>`;
     document.body.appendChild(div);
-    document.onclick = function(){
+    document.body.onclick = function(){
+        console.log(1)
         reMoveMexBox(type);
     };
 };
 function reMoveMexBox(type){
     document.getElementById("jump-box").remove();
-    document.onclick = null;
+    document.body.onclick = null;
     if(type===1){
         $('#password').focus();
     }else if(type===2){
         $('#repassword').focus();
-    }
+    };
 };
 
 // 密码是否合法
 function testPassword(id){
     var regex = /(?!^[0-9]+$)(?!^[A-z]+$)(?!^[^A-z0-9]+$)^.{6,16}$/;
     return regex.test($('#'+id).val());
-}
+};
 
-$('#resetpassword').click(function(){
-    if(testPassword("password")){
-        // 合法
-        if($('#password').val()===$('#repassword').val()){
-            // 审核相同，可以提交
-            
-        }else{
-            // 审核失败，密码不同
-            showTips(4,2);
-            return false;
-        }
-    }else{
-        // 不合法
-        showTips(3,1);
-        return false;
-    };
-});
