d71ea598fb8afaf6a7e644010a6d70f57bafedbe
maopixin <18364076997@163.com>
2018-03-13 16:41:08 +0800

    完善页面

--- 微信公众号-移动端-東联-静态页面/js/check.js
+++ 微信公众号-移动端-東联-静态页面/js/check.js
@@ -3,4 +3,18 @@ $('.check-box-item').click(function(){
 	$(this).parent().children().removeClass('active');
 	$(this).addClass('active');
 	$(this).parent().find(".select_txt").text('更多∨');
-})
+});
+
+//传入父级 #id 或者子集的.类名  为了保证切换的唯一性 建议传入父级id
+function changeActiveBtn(str){
+	var code = str.slice(0,1) === "#";
+	if(code){//id
+		$(str).children().click(function(){
+			$(this).addClass("active").siblings().removeClass("active");
+		});
+	}else{
+		$(str).click(function(){
+			$(this).addClass("active").siblings().removeClass("active");
+		})
+	};
+};
